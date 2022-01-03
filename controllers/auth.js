55d174e89343706723c44157c8f8b001be2517a0
Alex Vega Armijo <alexvega06.1995@gmail.com>
2022-01-03 02:07:58 -0300

    Google Sign in agregado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
-    const { response, request } = require("express");
+    const { response, request, json } = require("express");
     const Usuario = require("../models/usuario");
     const bcryptjs = require('bcryptjs');
     const { generarJWT } = require("../helpers/generarJWT");
+    const { googleVerify } = require("../helpers/google-verify");
+const { DefaultTransporter } = require("google-auth-library");
 
 
         const login = async (req, res = response) =>{
@@ -32,12 +34,6 @@
 
                 const token = await  generarJWT(usuario.id);
 
-
-
-
-
-
-
                 res.json({
                     msg: 'Login ok',
                     usuario,
@@ -53,6 +49,54 @@
 
         }
 
+        const googleSignIn = async (req, res = response) => {
+
+            const {id_token} = req.body;
+            
+            try {
+
+                const {correo,nombre,img} = await googleVerify( id_token )
+
+                let usuario = await Usuario.findOne({correo});
+
+                if(!usuario){
+                    const data ={
+                        nombre,
+                        correo,
+                        password: ':P',
+                        rol: 'USER_ROLE',
+                        img,
+                        google: true
+                    };
+
+
+                    usuario = new Usuario( data );
+                    await usuario.save();
+                }
+
+                if(!usuario.estado){
+                    return res.status(401).json({
+                        msg: 'Hable con el administrador, Usuario bloqueado'
+                    });
+                }
+               
+                const token = await generarJWT(usuario.id);
+
+                res.json({
+                usuario,
+                token,
+                });
+                
+            } catch (error) {
+                res.status(400).json({
+                    ok: false,
+                    msg: 'El token no se pudo verificar'
+                })
+            }
+
+        }
+
     module.exports = {
-        login
+        login,
+        googleSignIn
     }
